0900dd3a72280e9dafc160ca62190173315700ee
RAlanWright <wright.alan88@gmail.com>
2019-05-16 13:21:40 -0500

    Fixed DB issue by running drop and create in terminal, and set task.completed to add

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:G3tw0rkd0n3
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+
+# Must run python-sqlalchemy commands to refresh db
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -16,7 +18,6 @@ class Task(db.Model):
     def __init__(self, name):
         self.name = name
         self.completed = False
-
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -30,19 +31,20 @@ def index():
 
     tasks = Task.query.filter_by(completed=False).all()
     completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html', title="Get It Done!",
-        tasks=tasks, completed_tasks=completed_tasks)
+    return render_template('todos.html', title="Get It Done!", tasks=tasks,
+                           completed_tasks=completed_tasks)
 
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
+
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
     task.completed = True
-    db.session.delete(task)
+    db.session.add(task)
     db.session.commit()
-    return redirect('/')
 
+    return redirect('/')
 
 
 if __name__ == '__main__':
